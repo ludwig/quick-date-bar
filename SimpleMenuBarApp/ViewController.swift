09aaf4b54bbff2ea640893a3d39b0f47f414a91f
Luis Armendariz <luis.armendariz@gmail.com>
2023-04-16 04:31:03 -0700

    Initial Commit

---
+++ SimpleMenuBarApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SimpleMenuBarApp
+//
+//  Created by Luis Armendariz on 4/16/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
