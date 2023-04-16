6fa3cc7c93835deeed3872ede5e8a4c7fd05a9db
Luis Armendariz <luis.armendariz@gmail.com>
2023-04-16 08:50:11 -0700

    Add menu item to restore old pasteboard entry

--- SimpleMenuBarApp/StatusBarController.swift
+++ SimpleMenuBarApp/StatusBarController.swift
@@ -11,6 +11,8 @@ import Cocoa
 class StatusBarController: NSObject {
     private var statusItem: NSStatusItem
     private let menu = NSMenu()
+    private var restorePasteboardMenuItem: NSMenuItem?
+    private var oldPasteboardItem: NSPasteboardItem?
 
     override init() {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
@@ -19,13 +21,33 @@ class StatusBarController: NSObject {
     }
 
     private func setupMenu() {
-        let copyMenuItem = NSMenuItem(title: "Copy Today's Evernote Journal Prefix", action: #selector(copyString), keyEquivalent: "")
+        // https://stackoverflow.com/questions/49294949/disable-enable-nsmenu-item
+        menu.autoenablesItems = false
+
+        let copyMenuItem = NSMenuItem(
+            title: "Copy Today's Evernote Journal Prefix",
+            action: #selector(copyDateString),
+            keyEquivalent: ""
+        )
         copyMenuItem.target = self
         menu.addItem(copyMenuItem)
 
+        restorePasteboardMenuItem = NSMenuItem(
+            title: "Restore Old Pasteboard",
+            action: #selector(restoreOldPasteboardItem),
+            keyEquivalent: ""
+        )
+        restorePasteboardMenuItem?.target = self
+        restorePasteboardMenuItem?.isEnabled = false
+        menu.addItem(restorePasteboardMenuItem!)
+        
         menu.addItem(NSMenuItem.separator())
 
-        let quitMenuItem = NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q")
+        let quitMenuItem = NSMenuItem(
+            title: "Quit",
+            action: #selector(quitApp),
+            keyEquivalent: "q"
+        )
         quitMenuItem.target = self
         menu.addItem(quitMenuItem)
 
@@ -42,11 +64,42 @@ class StatusBarController: NSObject {
         return dateString
     }
 
-    @objc private func copyString() {
-        let pasteboard = NSPasteboard.general
+    @objc private func copyDateString() {
         let dateString = formattedDateString()
+        
+        // Save the current pasteboard contents
+        let pasteboard = NSPasteboard.general
+        let oldPasteboardItems = pasteboard.pasteboardItems ?? []
+        if let oldItem = oldPasteboardItems.first {
+            self.oldPasteboardItem = NSPasteboardItem()
+            for pasteboardType in oldItem.types {
+                if let pasteboardData = oldItem.data(forType: pasteboardType) {
+                    self.oldPasteboardItem?.setData(
+                        pasteboardData,
+                        forType: pasteboardType
+                    )
+                }
+            }
+            restorePasteboardMenuItem?.isEnabled = true
+        } else {
+            self.oldPasteboardItem = nil
+            restorePasteboardMenuItem?.isEnabled = false
+        }
+
+        // Write the date string to the pasteboard
         pasteboard.clearContents()
         pasteboard.setString(dateString, forType: .string)
+    }
+
+    @objc func restoreOldPasteboardItem() {
+        let pasteboard = NSPasteboard.general
+        pasteboard.clearContents()
+        
+        if let oldItem = oldPasteboardItem {
+            pasteboard.writeObjects([oldItem])
+        }
+        oldPasteboardItem = nil
+        restorePasteboardMenuItem?.isEnabled = false
     }
 
     @objc private func quitApp() {
