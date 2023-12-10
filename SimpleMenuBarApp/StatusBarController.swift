dca179618210c6d39280d43d3b6f594fb587b477
Luis Armendariz <luis.armendariz@gmail.com>
2023-12-10 02:29:38 -0800

    Refactor code and add more formats
    Added copyToClipboard() function to refactor
    some of the repeated code in the copyX() methods.
    
    Also implemented the following formats
    * ISO 8601
    * Hugo front matter template
    
    Defined dateTitle and yearWeekTitle as computed
    properties so that the menu items can update
    whenever the menu itself is opened.

--- SimpleMenuBarApp/StatusBarController.swift
+++ SimpleMenuBarApp/StatusBarController.swift
@@ -8,12 +8,25 @@
 import Foundation
 import Cocoa
 
-class StatusBarController: NSObject {
+class StatusBarController: NSObject, NSMenuDelegate {
     private var statusItem: NSStatusItem
     private let menu = NSMenu()
     private var restorePasteboardMenuItem: NSMenuItem?
     private var oldPasteboardItem: NSPasteboardItem?
+    private var copyDateMenuItem: NSMenuItem?
     private var copyYearWeekMenuItem: NSMenuItem?
+
+    private var dateFormat = "yyyy-MM-dd"
+    private var dateTitle: String {
+        let date = formattedDate(with: dateFormat)
+        return "Copy today's date as prefix (\(date))"
+    }
+
+    private var yearWeekFormat = "yyyy-'W'ww"
+    private var yearWeekTitle: String {
+        let yearWeek = formattedDate(with: yearWeekFormat)
+        return "Copy today's week prefix (\(yearWeek))"
+    }
 
     override init() {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
@@ -22,6 +35,8 @@ class StatusBarController: NSObject {
     }
 
     private func setupMenu() {
+        menu.delegate = self
+
         // https://stackoverflow.com/questions/49294949/disable-enable-nsmenu-item
         menu.autoenablesItems = false
 
@@ -44,16 +59,16 @@ class StatusBarController: NSObject {
         copyJournalPrefixMenuItem.target = self
         menu.addItem(copyJournalPrefixMenuItem)
 
-        let copyDateMenuItem = NSMenuItem(
-            title: "Copy today's date as prefix",
+        copyDateMenuItem = NSMenuItem(
+            title: dateTitle,
             action: #selector(copyDate),
             keyEquivalent: "2"
         )
-        copyDateMenuItem.target = self
-        menu.addItem(copyDateMenuItem)
+        copyDateMenuItem?.target = self
+        menu.addItem(copyDateMenuItem!)
 
         let copyTimeMenuItem = NSMenuItem(
-            title: "Copy now as time prefix",
+            title: "Copy now as time filename suffix",
             action: #selector(copyTime),
             keyEquivalent: "3"
         )
@@ -69,13 +84,20 @@ class StatusBarController: NSObject {
         menu.addItem(copyISO8601DateMenuItem)
 
         copyYearWeekMenuItem = NSMenuItem(
-            title: "Copy today's week prefix",
+            title: yearWeekTitle,
             action: #selector(copyYearWeek),
             keyEquivalent: "w"
         )
-        copyYearWeekMenuItem?.keyEquivalentModifierMask = [.command]
         copyYearWeekMenuItem?.target = self
         menu.addItem(copyYearWeekMenuItem!)
+
+        let copyHugoFrontMatterMenuItem = NSMenuItem(
+            title: "Copy Hugo front matter template",
+            action: #selector(copyHugoFrontMatterTemplate),
+            keyEquivalent: "f"
+        )
+        copyHugoFrontMatterMenuItem.target = self
+        menu.addItem(copyHugoFrontMatterMenuItem)
 
         menu.addItem(NSMenuItem.separator())
 
@@ -89,20 +111,29 @@ class StatusBarController: NSObject {
 
         statusItem.menu = menu
         statusItem.button?.image = NSImage(named: "calendarIcon")
-
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(updateMenu),
-            name: NSMenu.willSendActionNotification,
-            object: nil
-        )
     }
 
-    @objc private func updateMenu() {
+    func menuWillOpen(_ menu: NSMenu) {
+        copyDateMenuItem?.title = dateTitle
+        copyYearWeekMenuItem?.title = yearWeekTitle
+    }
+
+    private func formattedDate(with format: String) -> String {
         let formatter = DateFormatter()
-        formatter.dateFormat = "ww"
-        let weekNumberString = formatter.string(from: Date())
-        copyYearWeekMenuItem?.title = "Copy today's week prefix (W\(weekNumberString))"
+        formatter.dateFormat = format
+        return formatter.string(from: Date())
+    }
+
+    private func formattedISO8601Date() -> String {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [
+            .withInternetDateTime,
+            .withDashSeparatorInDate,
+            .withColonSeparatorInTime,
+            .withColonSeparatorInTimeZone,
+        ]
+        formatter.timeZone = TimeZone.current
+        return formatter.string(from: Date())
     }
 
     private func formattedJournalPrefix() -> String {
@@ -116,49 +147,48 @@ class StatusBarController: NSObject {
 
     @objc private func copyJournalPrefix() {
         let prefix = formattedJournalPrefix()
-        saveCurrentPasteboardContents()
-        let pasteboard = NSPasteboard.general
-        pasteboard.clearContents()
-        pasteboard.setString(prefix, forType: .string)
+        copyToClipboard(prefix)
     }
 
     @objc private func copyDate() {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        let dateString = formatter.string(from: Date())
-        saveCurrentPasteboardContents()
-        let pasteboard = NSPasteboard.general
-        pasteboard.clearContents()
-        pasteboard.setString(dateString, forType: .string)
+        let dateString = formattedDate(with: dateFormat)
+        copyToClipboard(dateString)
     }
 
     @objc private func copyTime() {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy_MM_dd__HH_mm_ss"
         let timeString = formatter.string(from: Date())
-        saveCurrentPasteboardContents()
-        let pasteboard = NSPasteboard.general
-        pasteboard.clearContents()
-        pasteboard.setString(timeString, forType: .string)
-    }
-
-    @objc private func copyYearWeek() {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-'W'ww"
-        let yearWeekString = formatter.string(from: Date())
-        saveCurrentPasteboardContents()
-        let pasteboard = NSPasteboard.general
-        pasteboard.clearContents()
-        pasteboard.setString(yearWeekString, forType: .string)
+        copyToClipboard(timeString)
     }
 
     @objc private func copyISO8601Date() {
-        let formatter = ISO8601DateFormatter()
-        let dateString = formatter.string(from: Date())
+        let dateString = formattedISO8601Date()
+        copyToClipboard(dateString)
+    }
+
+    @objc private func copyYearWeek() {
+        let yearWeekString = formattedDate(with: yearWeekFormat)
+        copyToClipboard(yearWeekString)
+    }
+
+    @objc private func copyHugoFrontMatterTemplate() {
+        let template = """
+        ---
+        title: "Your title here"
+        date: \(formattedISO8601Date())
+        tags: []
+        draft: false
+        ---
+        """
+        copyToClipboard(template)
+    }
+
+    private func copyToClipboard(_ string: String) {
         saveCurrentPasteboardContents()
         let pasteboard = NSPasteboard.general
         pasteboard.clearContents()
-        pasteboard.setString(dateString, forType: .string)
+        pasteboard.setString(string, forType: .string)
     }
 
     private func saveCurrentPasteboardContents() {
